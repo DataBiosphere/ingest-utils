9ac13c2e764e0a6562c1e18b250ed208e0b52310
Raaid Arshad <rarshad@broadinstitute.org>
2020-09-23 17:09:06 -0400

    move post process from class args to a function to override

--- ingest-scio-utils/src/main/scala/org/broadinstitute/monster/common/ScioApp.scala
+++ ingest-scio-utils/src/main/scala/org/broadinstitute/monster/common/ScioApp.scala
@@ -9,19 +9,20 @@ import com.spotify.scio.{ContextAndArgs, ScioResult}
 /**
   * @param parser implicit parser doesn't need to be passed, derived from Args type
   * @param help help generated from the Args type
-  * @param postProcess a post processing function if so desired
   * @tparam Args an Args case class with input definitions and help messages
   */
 abstract class ScioApp[Args](
   implicit parser: Parser[Args],
-  help: Help[Args],
-  postProcess: ScioResult => Unit = _ => ()
+  help: Help[Args]
 ) {
   /** Builder which can convert command-line args into a pipeline. */
   def pipelineBuilder: PipelineBuilder[Args]
 
   /** Formatter for CLP arguments which retains the formatting of the Scala name. */
   private val argFormatter: Formatter[Name] = name => name.name
+
+  /** a post processing function if so desired that operates on a ScioResult */
+  def postProcess: ScioResult => Unit = _ => ()
 
   /**
     * Entry-point for scio CLPs.
